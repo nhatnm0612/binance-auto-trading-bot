100af954a57baad0733149b2e64bffb2e0e69195
Nhat Nguyen Minh <nhatnguyenminh061289@gmail.com>
2018-02-04 17:05:04 +0700

    Small change

--- CryptoCurrency.py
+++ CryptoCurrency.py
@@ -141,3 +141,16 @@ class Binance:
             if info["symbol"] == self.symbol:
                 return info
 
+
+if __name__ == "__main__":
+    APIKey   = ""
+    Secret   = ""
+    coin1    = "BNB"
+    coin2    = "USDT"
+    BNBUSDT  = Binance(APIKey, Secret, coin1, coin2)
+    while True:
+        try:
+            BNBUSDT.exchange_coin2(SELLING_PRICE = 9.80)
+            BNBUSDT.exchange_coin1(BUYING_PRICE = 9.73)
+        except ValueError:
+            time.sleep(5)
